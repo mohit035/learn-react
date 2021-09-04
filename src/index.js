b7b7c68bc8ea6ed7e6321364c444137e914730a9
Mohit khadayat <mohitkhadayat06@gmail.com>
2021-09-04 16:57:28 +0545

    intro-jsx&babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Hello World!</h1>,
  document.getElementById('root') );
+
+// introduction to jsx -> javascript extension,javascript xml
+// the expression which looks like html but it is jsx 
+// to use jsx we have to import react module
+// react module is use to create to create html element 
+// babel is use to compile the jsx code to the browser understand traditional js
+var h1 = document.createElement('h1');
+h1.innerHTML = "Mohit Khadayat";
+document.getElementById('root').appendChild(h1)
